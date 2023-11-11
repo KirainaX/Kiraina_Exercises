2dea967c7c4700ae035495bd515f6bdbb35412ef
KirainaX <bouzridazakaria@email.com>
2023-11-11 04:21:37 -0800

    free palestine

---
+++ 0-hello_world/4-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.*/
+
+int main()
+{
+    printf("and that piece of art is useful/"" - Dora korpar, 2015-10-19");
+
+    return 0;
+}
