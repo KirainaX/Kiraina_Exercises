2f9401dc4ecf98ad7bd66b71cd508e0592e30136
KirainaX <bouzridazakaria@email.com>
2023-11-13 01:29:55 -0800

    free palestine

---
+++ 2-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*Write a function that adds two integers and returns the result.*/
+
+int add(int a, int b)
+{
+	int result;
+
+	return result = a + b;
+}
+
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
