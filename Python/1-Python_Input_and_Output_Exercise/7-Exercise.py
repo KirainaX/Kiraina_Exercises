b27dba406903b8f365a1c3f7ae4be9f54ee97c00
KirainaX <bouzridazakaria@email.com>
2023-12-09 15:56:42 +0100

    free palestine

--- Python/1-Python_Input_and_Output_Exercise/7-Exercise.py
+++ Python/1-Python_Input_and_Output_Exercise/7-Exercise.py
@@ -1,12 +1,27 @@
 # Write a program to take three names as input from a user in the single input() function call.
 names = []
+j = 0
+row = 1
 num = int(input("Enter how many names u will enter: "))
-print("Enter three string Emma Jessa Kelly")
-for i in range(num):
-    print("Name: ", end="")
+idx = num
+for i in range(1, num + 1):
+    print("Name{:d}: ".format(i), end="")
     itm = input()
     names.append(itm)
 
+print("Enter three string ",end="")
 for i in names:
-    for j in range(1, num + 1):
-        print("Name",j,": ",i)
+    print(i, end=" ")
+print("")
+
+print("====================================", names)
+for i in names:
+    for j in range(row, num - idx + 2):
+        print("Name{:d}: ".format(j), i)
+        idx = idx + 1
+        row = row + 1
+
+'''str1, str2, str3 = input("Enter three string ").split()
+print('Name1:', str1)
+print('Name2:', str2)
+print('Name3:', str3)'''
