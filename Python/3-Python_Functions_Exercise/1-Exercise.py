e9bac491de55ca1994dd9892e2b8c177bb6c81e2
KirainaX <bouzridazakaria@email.com>
2023-12-09 16:09:01 +0100

    free palestine

---
+++ Python/3-Python_Functions_Exercise/1-Exercise.py
@@ -0,0 +1,10 @@
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def name_age():
+    names = input("Enter your name: ")
+    ages = int(input("Enter your age: "))
+
+    print(names, ages)
+
+
+print_name_age = name_age()
