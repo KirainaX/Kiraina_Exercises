fd4b382a38eddebc6b6da68dabb40b490ea36ea2
KirainaX <bouzridazakaria@email.com>
2023-12-09 18:26:38 +0100

    free palestine

---
+++ Python/4-Python_String_Exercise/1.2-Exercise.py
@@ -0,0 +1,28 @@
+# Write a program to create a new string made of the middle three characters of an input string.
+
+'''
+# mine
+str1 = "JaSonAy"
+# str1 = "JhonDipPeta"
+len = len(str1)
+mid = int(len / 2) - 1
+for i in range(mid, len):
+    if i == (mid + 3):
+        break
+    else:
+        print(str1[i], end="")
+print("")
+'''
+# ==============================================
+def get_middle_three_chars(str1):
+    print("Original String is", str1)
+
+    # first get middle index number
+    mi = int(len(str1) / 2)
+
+    # use string slicing to get result characters
+    res = str1[mi - 1:mi + 2]
+    print("Middle three chars are:", res)
+
+get_middle_three_chars("JhonDipPeta")
+get_middle_three_chars("JaSonAy")
