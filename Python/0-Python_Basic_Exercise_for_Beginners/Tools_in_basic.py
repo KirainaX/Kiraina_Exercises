9b7e30c12e1502eed6ea69000e3436098f90656f
KirainaX <bouzridazakaria@email.com>
2023-12-14 09:43:11 +0100

    free palestine

---
+++ Python/0-Python_Basic_Exercise_for_Beginners/Tools_in_basic.py
@@ -0,0 +1,6 @@
+# all u need in basic in python
+str1 = "shff"
+print()
+input()
+len()
+str1.append()
--- Python/7-Python_Dictionary_Exercise/Tools_in_dictionary.py
+++ Python/7-Python_Dictionary_Exercise/Tools_in_dictionary.py
@@ -1,6 +1,6 @@
-# Tools i need
+# Tools i need for dictionary
 
-dict1 = {}
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
 dict1 = zip()
 dict1 = dict()
@@ -9,4 +9,3 @@ dict1 = dict.fromkeys()
 dict1.pop()
 dict1.keys()
 dict1.values()
-
