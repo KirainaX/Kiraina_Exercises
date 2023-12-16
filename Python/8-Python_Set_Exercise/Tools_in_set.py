0b3d46c3f1a52d8576bc026e6e42f7e4a766043e
KirainaX <bouzridazakaria@email.com>
2023-12-16 09:16:05 +0100

    free palestine

---
+++ Python/8-Python_Set_Exercise/Tools_in_set.py
@@ -0,0 +1,12 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set(set1)
+set1.update() #EX1
+set1.intersection(set2) #EX2
+set1.union(set2) #EX3
+set1.difference_update(set2) #EX4 / EX5
+set1.symmetric_difference(set2) #EX6
+set1.isdisjoint(set2) #EX7
+set1.symmetric_difference_update(set2) #EX8
+set1.intersection_update(set2) #EX9
