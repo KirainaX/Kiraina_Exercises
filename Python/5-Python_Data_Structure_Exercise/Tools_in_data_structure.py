fc51d9aa98b06cb84a2c7a1b0a154186fcacc791
KirainaX <bouzridazakaria@email.com>
2023-12-16 09:51:18 +0100

    free palestine

---
+++ Python/5-Python_Data_Structure_Exercise/Tools_in_data_structure.py
@@ -0,0 +1,18 @@
+# all u need in data structure
+l1 = [3, 6, 9, 12, 15, 18, 21]
+l2 = [4, 8, 12, 16, 20, 24, 28]
+
+rm_l1 = l1.pop(4) #EX2
+length = len() #EX3
+indexes = slice() #EX3
+L1 = list(reversed()) #EX3
+zipped = zip() #EX5
+result_list = set(zipped) #EX5
+L1.remove(3) #EX6
+L3 = L1.issuperset() #EX7
+L4 = L2.issubset() #EX7
+L1..clear() #EX7
+L1.values() #EX8 / #EX9
+L1.append() #EX9 / #EX10
+tuple_list = tuple() #EX10
+list_sample = list() #EX10
