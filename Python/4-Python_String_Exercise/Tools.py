4160a16f99d8cd0e2ae9e5678f149405880d7927
KirainaX <bouzridazakaria@email.com>
2023-12-16 10:34:23 +0100

    free palestine

---
+++ Python/4-Python_String_Exercise/Tools.py
@@ -0,0 +1,18 @@
+# all u need to know
+
+len() #EX1.1 / EX1.2 / EX6 / EX11
+int() #EX2 / EX3 /EX6
+append() #EX4 / EX14 / EX17 / EX18
+islower() #EX4
+join() #EX4 / EX16 / EX18
+isalpha() #EX5 / EX17 / EX18
+isdigit() #EX5 / EX9 / EX16 / EX17
+lower() #EX8
+count() #EX8
+count() #EX10
+rfind() #EX12
+split() #EX13 / EX17
+translate() #EX15
+maketrans() #EX15
+any() #EX17
+replace() #EX18
