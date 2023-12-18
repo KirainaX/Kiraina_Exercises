8e8d27e816e3ef12f94f93384c8ec4a68f3e6f95
KirainaX <bouzridazakaria@email.com>
2023-12-18 18:38:10 +0100

    free palestine

---
+++ Python/4-Python_String_Exercise/1-count_char.py
@@ -0,0 +1,15 @@
+# Write a Python program to count the number of characters (character frequency) in a string.
+def count_char(str1):
+     new_str = {}
+     for i in str1:
+          if i in new_str:
+               new_str[i] += 1
+          else:
+               new_str[i] = 1
+     return new_str
+        
+
+str1 = "google.com"
+
+result = count_char(str1)
+print(result)
