63709d0e05fc945a7f0c2d515e0121a96c4f2e09
KirainaX <bouzridazakaria@email.com>
2023-12-20 12:23:06 +0100

    free palestine

--- Python/2-conditional_statements_and_loops/tools.py
+++ Python/2-conditional_statements_and_loops/tools.py
@@ -7,3 +7,8 @@ bouth.append(i) = tzid chy haja f list ra9am string ...
 ex.upper() = kat7awalik ay horof s4ar ila 7orof kbar
 ex.lower() = kat7awalik ay 7orof kbar ila 7orof s4ar
 """
+""""
+==> RANDOM <==
+import random /first
+random.randint(1, 9) = kat3zalik ra9am mabin 1 w 9 but 4ir integer
+"""
