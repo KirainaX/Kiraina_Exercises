9f2ca3cda760b13ee062caf16cfe207696dc0915
KirainaX <bouzridazakaria@email.com>
2023-12-25 07:59:38 +0100

    free palestine

--- Python/2-conditional_statements_and_loops/Exercise-3.py
+++ Python/2-conditional_statements_and_loops/Exercise-3.py
@@ -6,8 +6,12 @@ def guess_number():
     player_num = int(input("Enter number between 1 and 10 that u thik is the right number: "))
     if player_num > 10:
         print("The number u enter is greater than 10")
+        print("guess again")
+        guess_number()
     elif player_num < 1:
         print("The number u enter is less than 1")
+        print("guess again")
+        guess_number()
     else:
         if random_num == player_num:
             print("Well guessed!")
@@ -17,7 +21,7 @@ def guess_number():
 
 guess_number()
 print("Do you want to play again?")
-again = input("enter 'Y' for Yes or 'N' for No: ")
+again = input("enter 'y' for Yes or 'n' for No: ")
 if (again.lower() == 'y') or (again == 'Y'):
     guess_number()
 else:
