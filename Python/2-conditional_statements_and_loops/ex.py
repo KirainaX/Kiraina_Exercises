05aa6187a64c6b403fda204b8e5694c55482fcc6
KirainaX <bouzridazakaria@email.com>
2023-12-25 12:37:56 +0100

    free palestine

--- Python/2-conditional_statements_and_loops/ex.py
+++ Python/2-conditional_statements_and_loops/ex.py
@@ -50,5 +50,7 @@ Validation :
         * Minimum length 6 characters.
         * Maximum length 16 characters.
 ==> EX16 <==
-
+Write a Python program to find numbers between 100 and 400 (both included)
+where each digit of a number is an even number. The numbers obtained should
+be printed in a comma-separated sequence.
 """
