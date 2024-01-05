eb07587699300eeda0eaab247a12af2418ead8be
KirainaX <bouzridazakaria@email.com>
2024-01-05 18:16:44 +0100

    free palestine

---
+++ Python/projects/1-mad_libs.py
@@ -0,0 +1,18 @@
+"""
+
+[adjective] [noun].
+"""
+adjective1 = input("Enter a adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective2 = input("Enter a adjective: ")
+noun2 = input("Enter a noun: ")
+adverb = input("Enter a adverb: ")
+verb2 = input("Enter a verb: ")
+adjective3 = input("Enter a adjective: ")
+verb3 = input("Enter a verb: ")
+verb4 = input("Enter a verb: ")
+adjective4 = input("Enter a adjective: ")
+noun3 = input("Enter a noun: ")
+
+print("One {} day, I went to the {} and {} a {} {}.\nIt was so {} that I {} {}.\nEveryone {} and {} because of my {} {}.".format(adjective1, noun1, verb1, adjective2, noun2, adverb, verb2, adjective3, verb3, verb4, adjective4, noun3))
