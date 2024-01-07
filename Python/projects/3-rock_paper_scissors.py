88b301b77252615bfbdbf15a54d6ec51f3b4bf87
KirainaX <bouzridazakaria@email.com>
2024-01-07 12:42:14 +0100

    free palestine

--- Python/projects/3-rock_paper_scissors.py
+++ Python/projects/3-rock_paper_scissors.py
@@ -5,34 +5,37 @@ import random
 def rock_paper_scissors():
     
     # List of possible choices: 'r' for Rock, 'p' for Paper, 's' for Scissors
-    list_guess = ["r", "p", "s"]
+    list_choice = ["r", "p", "s"]
     
     # Randomly select a choice for the computer opponent
-    random_guess = random.choice(list_guess)
+    random_choice = random.choice(list_choice)
 
     # Get the player's choice and ensure it is valid
     player_choice = input("==> Are you ready to play?\nChoose one of the options\n'r' for Rock and 'p' for Paper and 's' for Scissors: ")
     player_choice = player_choice.lower()
 
     # Validate the player's input
-    while player_choice not in list_guess:
+    while player_choice not in list_choice:
         print("")
         player_choice = input("==> You did not enter an option. Please choose one of the options\n'r' for Rock and 'p' for Paper and 's' for Scissors: ")
         player_choice = player_choice.lower()
 
     # Compare the player's choice with the computer's choice to determine the winner
-    while player_choice in list_guess:
-        if player_choice == random_guess:
+    while player_choice in list_choice:
+        if player_choice == random_choice:
             print("")
             print("==> Draw -_-")
+            print("You chose {} And Bot chose {}".format(player_choice, random_choice))
             break
-        elif (player_choice == 'r' and random_guess == 's') or (player_choice == 'p' and random_guess == 'r') or (player_choice == 's' and random_guess == 'p'):
+        elif (player_choice == 'r' and random_choice == 's') or (player_choice == 'p' and random_choice == 'r') or (player_choice == 's' and random_choice == 'p'):
             print("")
             print("==> You Win :)")
+            print("You chose {} And Bot chose {}".format(player_choice, random_choice))
             break
         else:
             print("")
             print("==> You Loss :(")
+            print("You chose {} And Bot chose {}".format(player_choice, random_choice))
             break
 
 # Call the Rock-Paper-Scissors function to play the game
