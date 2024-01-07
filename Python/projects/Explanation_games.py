b2dbf12a3be3fe422b145785ee3126a72d136799
KirainaX <bouzridazakaria@email.com>
2024-01-07 14:12:54 +0100

    free palestine

---
+++ Python/projects/Explanation_games.py
@@ -0,0 +1,22 @@
+"""
+Project1:
+This code allows users to create their own customized stories by providing different
+words for each part of speech, resulting in a unique and often humorous narrative.
+"""
+"""
+Project2:
+This code essentially creates a number guessing game where the player needs to
+guess a random number between 1 and 10. After each round, the player is given the
+option to play again or exit the game. The game input is validated to ensure it's a valid
+number within the specified range.
+"""
+"""
+Project3:
+This code defines a simple Rock-Paper-Scissors game where the player can play
+multiple rounds and choose whether to continue playing after each round. The code
+uses functions and loops to organize the game logic and user interaction.
+"""
+"""
+Project4:
+
+"""
--- Python/projects/README.md
+++ Python/projects/README.md
@@ -1,1 +1,15 @@
 # Explanation of the game
+'''count = 0
+another_list = {}
+
+for i in anime_list:
+    count += 1
+print(count)
+
+for i in anime_list:
+    if i in another_list:
+        another_list[i] += 1
+    else:
+        another_list[i] = 1
+
+print(another_list)'''
