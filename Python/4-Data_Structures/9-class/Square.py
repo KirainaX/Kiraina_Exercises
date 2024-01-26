6048bae3bd5b3a43bb58316f28c0c314b7ceb762
KirainaX <bouzridazakaria@email.com>
2024-01-26 16:38:05 +0100

    free palestine

--- Python/4-Data_Structures/9-class/Square.py
+++ Python/4-Data_Structures/9-class/Square.py
@@ -1,27 +1,34 @@
-class Square(object):
-    def __init__(self, size = 0):
+class Square:
+    def __init__(self, size, position=(0, 0)):
         self.size = size
+
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
+    @property
+    def position(self):
+        return (self.__position)
+
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if tuple(value) < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def area(self):
         return self.__size ** 2
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+    def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
+        
